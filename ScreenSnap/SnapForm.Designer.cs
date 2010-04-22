a447cda5d048db3dba155e59bbeb45666e2bf24c
Nathan Reed <reednj@gmail.com>
2010-04-22 18:02:02 +0800

    Take a screenshot and save to a .png file on the desktop.  + code not so great, but rest is working well

--- ScreenSnap/SnapForm.Designer.cs
+++ ScreenSnap/SnapForm.Designer.cs
@@ -28,34 +28,28 @@ namespace ScreenSnap
         /// </summary>
         private void InitializeComponent()
         {
-            this.CaptureButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // CaptureButton
-            // 
-            this.CaptureButton.Location = new System.Drawing.Point(12, 12);
-            this.CaptureButton.Name = "CaptureButton";
-            this.CaptureButton.Size = new System.Drawing.Size(207, 41);
-            this.CaptureButton.TabIndex = 0;
-            this.CaptureButton.Text = "Capture Screen";
-            this.CaptureButton.UseVisualStyleBackColor = true;
-            this.CaptureButton.Click += new System.EventHandler(this.CaptureButton_Click);
             // 
             // SnapForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 66);
-            this.Controls.Add(this.CaptureButton);
+            this.ClientSize = new System.Drawing.Size(952, 624);
             this.Name = "SnapForm";
+            this.Opacity = 0.3;
             this.Text = "ScreenSnap";
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SnapForm_MouseUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SnapForm_MouseMove);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SnapForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SnapForm_MouseDown);
+            this.Load += new System.EventHandler(this.SnapForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button CaptureButton;
+
     }
 }
 
--- ScreenSnap/SnapForm.cs
+++ ScreenSnap/SnapForm.cs
@@ -14,26 +14,101 @@ namespace ScreenSnap
 {
     public partial class SnapForm : Form
     {
+
+        bool mouseDown = false;
+        Point mouseDownPoint = Point.Empty;
+        Point mousePoint = Point.Empty;
+        Rectangle prevRect = new Rectangle();
+
         public SnapForm()
         {
             InitializeComponent();
+            
+            this.WindowState = FormWindowState.Maximized;
+            
+            this.Cursor = Cursors.Cross;
+            this.Text = string.Empty;
+            this.ControlBox = false;
+            this.ShowInTaskbar = false;
         }
 
-        private void CaptureButton_Click(object sender, EventArgs e)
-        {
-            this.CaptureScreen();
-        }
-
-        private void CaptureScreen()
+        private void CaptureScreen(Rectangle r)
         {
             string DesktopDir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
 
-            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Bitmap bitmap = new Bitmap(Math.Abs(r.Width), Math.Abs(r.Height)); 
             Graphics graphics = Graphics.FromImage(bitmap as Image);
-            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            graphics.CopyFromScreen(r.Left, r.Top, 0, 0, bitmap.Size);
             
-            
-            bitmap.Save(Path.Combine(DesktopDir, "test.png"), ImageFormat.Png);
+            TimeSpan FileNumber = (DateTime.Now - DateTime.Today);
+            bitmap.Save(Path.Combine(DesktopDir, String.Format("snap-{0}.png", Math.Round(FileNumber.TotalSeconds))), ImageFormat.Png);
         }
+
+        private void SnapForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mouseDown == false) {
+                return;
+            }
+
+            mousePoint = e.Location;
+            
+            Point startPoint = this.PointToScreen(mouseDownPoint);
+            Point endPoint = this.PointToScreen(mousePoint);
+
+            int Top = Math.Min(startPoint.Y, endPoint.Y);
+            int Left = Math.Min(startPoint.X, endPoint.X);
+            int Bottom = Math.Max(startPoint.Y, endPoint.Y);
+            int Right = Math.Max(startPoint.X, endPoint.X);
+
+            Rectangle rect = new Rectangle(Left, Top, Right - Left, Bottom - Top);
+
+            ControlPaint.DrawReversibleFrame(prevRect, Color.Black, FrameStyle.Dashed);
+            ControlPaint.DrawReversibleFrame(rect, Color.Black, FrameStyle.Dashed);
+
+            prevRect = rect;
+        }
+
+        private void SnapForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(mouseDown == true) {
+                mouseDown = false;
+                this.CaptureScreen(prevRect);
+                
+                ControlPaint.DrawReversibleFrame(prevRect, Color.Black, FrameStyle.Dashed);
+            }
+
+            this.Close();
+        }
+
+        private void SnapForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(mouseDown == false) {
+                prevRect = new Rectangle();
+
+                mouseDown = true;
+                mousePoint = mouseDownPoint = e.Location;
+            }
+        }
+
+        private void SnapForm_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SnapForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape) {
+                this.Close();
+            }
+        }
+
+
+
     }
 }
