b56253668e5e9cad5309e0e88e048060ee331282
Nathan Reed <reednj@gmail.com>
2010-04-22 15:51:12 +0800

    Initial Commit

---
+++ ScreenSnap/SnapForm.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ScreenSnap
+{
+    public partial class SnapForm : Form
+    {
+        public SnapForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
